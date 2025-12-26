2142b0aad9206fee959fad52cfb3eed55b18fc4d
chakvetadzegio1-alt <chakvetadze.gio1@gmail.com>
2025-12-26 17:09:37 +0400

    commet

---
+++ level01/homework/hw.py
@@ -0,0 +1,65 @@
+# ორ რიცხვს შორის თითოეულ მათემატიკურ ოპერაციაზე 5-5 მაგალითი (+,-,*,/) და დაპრინტეთ
+
+print(7+7)
+print(9+9)
+print(2+2)
+print(1+1)
+print(6+3)
+
+print(120-34)
+print(290-20)
+print(863-21)
+print(123-79)
+print(344-56)
+
+print(2489*100)
+print(3289*127)
+print(9067*169)
+print(1234*659)
+print(3456*5)
+
+print(15/3)
+print(81/9)
+print(36/9)
+print(10/2)
+print(56/8)
+
+# 2)ცვლადები სადაც იქნება თქვენი სახელი, თქვენი გვარი,
+#  თქვენი ასაკი და დაბეჭდეთ ერთ დიდ წინადადებაში
+
+name = "George"
+last_name = "Chakvetadze"
+age = 12
+
+print(name, last_name, age)
+
+# 3)შექმენი 9 ცვლადი, 3 ცვლადი იყოს Str, 3 ცვლადი int, 3 ცვლადი float,
+#  და შემდეგ გამოიტანე ტერმინალში 9-ვე ცვლადი print-ის მეშვეობით.
+
+age = "76"
+name = "george"
+size = "67"
+
+number = 90
+year = 2025
+score = 34
+
+wildth = 35.6
+height = 1.52
+temperature = 89.38
+
+print(age)
+print(name)
+print(size)
+
+print(wildth)
+print(height)
+print(temperature)
+
+print(number)
+print(year)
+print(score)
+
+
+
+
