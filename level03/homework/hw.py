d9221f5f778732bf668a7719209f62c0929a871d
chakvetadzegio1-alt <chakvetadze.gio1@gmail.com>
2026-01-03 17:06:06 +0400

    commet

---
+++ level03/homework/hw.py
@@ -0,0 +1,21 @@
+# 1)მომხარებელს შემოაყვანინე სახელი, გვარი, ასაკი და საყვარელი მანქანა.
+
+# საბოლოოდ დაბეჭდე ერთი დიდი წინადადება ამ მონაცემებით
+
+name = input("Enter your name")
+last_name = input("Enter your last_name")
+age = input("Enter your age")
+car = input("Enter your car")
+print(name, last_name, age, car)
+
+# 2)მომხმარებელს შემოაყვანინე რომელი პროგრამირების აკადემია მიაჩნია საუკეთესოდ და  შემდეგ დაბეჭდე ეს პასუხი.
+
+name = input("Enter your best programer akademy ")
+print("GOA")
+
+# 3)მომხმარებელს შემოაყვანინე საყვარელი სპორტი და დაბეჭდე ის შემდეგ ტერმინალში.
+
+sport = input("Enter your sport")
+print("Football")
+
+
